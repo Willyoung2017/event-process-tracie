2760b8f589f83478773c4cc5fa55ae37ac90a689
Yanlin Feng <fengyanlin@pku.edu.cn>
2021-11-24 23:12:01 -0500

    Implement query event SRL/coreference, add query events to story chain

--- code/extract_event_chains.py
+++ code/extract_event_chains.py
@@ -5,7 +5,10 @@ from nltk.stem.snowball import SnowballStemmer
 import json
 import os
 import argparse
+from nltk.stem import WordNetLemmatizer
+from difflib import SequenceMatcher
 from tqdm import tqdm
+from typing import Dict, List
 
 TMP_PATH = 'data/chains/tmp.txt'
 
@@ -20,6 +23,7 @@ class SRLExtractor:
         self.srl = Predictor.from_path(self.srl_model_url,
                                        cuda_device=0 if use_cuda else -1)
         self.stemmer = self.stemmer_class('english')
+        self.lemmatizer = WordNetLemmatizer()
 
         make_dir(self.sp_path)
         self.sp_fout = open(self.sp_path, 'w')
@@ -27,14 +31,19 @@ class SRLExtractor:
     def free(self):
         self.sp_fout.close()
 
-    def select_verb(self, srl_dic):
+    def select_verb(self, srl_dic, sentence):
         """select the most appropriate frame from the predicted frames"""
         words = srl_dic['words']
         stem = ' '.join([self.stemmer.stem(w.lower()) for w in words])
         frames = srl_dic['verbs']
         verbs = [d['verb'] for d in frames]
-
-        if any(f'{x} going to' in stem for x in ['was', 'were', 'is', 'are']):
+        if len(frames) == 0:
+            self.sp_fout.write('[SRL]\n')
+            self.sp_fout.write(f'sentence: {sentence}\n')
+            self.sp_fout.write(f'SRL: {srl_dic}\n')
+            self.sp_fout.write(f'\n')
+            return None
+        elif any(f'{x} going to' in stem for x in ['was', 'were', 'is', 'are']):
             return frames[0]['verb']
         else:
             frames.sort(key=lambda d: sum(1 for tag in d['tags'] if tag != 'O'),
@@ -84,6 +93,7 @@ class SRLExtractor:
                 res = [event1, event2]
             else:
                 res = [sentence]
+        self.sp_fout.write(f'[SPLIT_TMP]\n')
         self.sp_fout.write(f'Input: {sentence}\n')
         self.sp_fout.write(f'Output: {res}\n')
         self.sp_fout.write(f'\n')
@@ -107,25 +117,53 @@ class SRLExtractor:
         res = []
         assert len(srl_results) == len(events)
         for srl, e in zip(srl_results, events):
-            if len(srl['verbs']) == 0:
-                print(f'Event [{e}] SRL failed')
+            verb = self.select_verb(srl, e)
+            if verb is None:
                 continue
-            verb = self.select_verb(srl)
             res.append({'verb': verb, 'event': e})
+        assert len(res) >= 1
         return res
 
     def parse_query_events(self, string):
         string = string.strip()
-        for temp_rel in ['starts before', 'starts after',
-                         'ends before', 'ends after']:
-            if temp_rel in string:
-                e1, e2 = string.split(temp_rel)
-                e1 = e1.replace('.', '').strip()
-                e2 = e2.replace('.', '').strip()
-                return e1, e2, temp_rel
+        for comparator in ['starts', 'ends']:
+            for temp in ['before', 'after']:
+                temp_rel = f'{comparator} {temp}'
+                if temp_rel in string:
+                    e1, e2 = string.split(temp_rel)
+                    e1 = e1.strip().strip('.')
+                    e2 = e2.strip().strip('.')
+                    srl1, srl2 = self.srl.predict_batch_json([{'sentence': e1}, {'sentence': e2}])
+                    v1 = self.select_verb(srl1, e1)  # v1 == None if SRL failed
+                    v2 = self.select_verb(srl2, e2)  # v2 == None if SRL failed
+                    e1 = e1 + ' ' + comparator  # event1 -> event1 starts/ends
+                    e1 = {'verb': v1, 'event': e1}
+                    e2 = {'verb': v2, 'event': e2}
+                    return e1, e2, temp_rel
         raise ValueError('Invalid input')
 
-    def process_file(self, input_path, output_path):
+    def locate_event(self, event: dict, event_chain: List[dict]):
+        """A simple event coreference system based on stemming and longest-common-subsequence matching"""
+        assert len(event_chain) >= 1
+        event_tok = nltk.word_tokenize(event['event'].lower())
+        chain_tok = [nltk.word_tokenize(d['event'].lower()) for d in event_chain]
+        event_stem = [self.stemmer.stem(self.lemmatizer.lemmatize(w, 'v')) for w in event_tok]
+        chain_stem = [[self.stemmer.stem(self.lemmatizer.lemmatize(w, 'v')) for w in e] for e in chain_tok]
+        scores = []
+        for e_chain in chain_stem:
+            matcher = SequenceMatcher(a=event_stem, b=e_chain)
+            scores.append(sum(block.size for block in matcher.get_matching_blocks()))
+        max_score = max(scores)
+        pos = scores.index(max_score)
+        if (max_score / len(event_stem)) <= 0.5:
+            self.sp_fout.write('[COREF]\n')
+            self.sp_fout.write(f'event: {event["event"]}\n')
+            self.sp_fout.write(
+                f'chain: {[("***" * int(i == pos)) + e["event"] for i, e in enumerate(event_chain)]}\n')
+            self.sp_fout.write(f'\n')
+        return pos
+
+    def process_file(self, input_path, output_path, is_test):
         with open(input_path, 'r') as fin:
             lines = [line.rstrip() for line in fin]
 
@@ -136,8 +174,26 @@ class SRLExtractor:
                 str_story, str_answer = line.split('answer:')
                 qe1, qe2, temp_rel = self.parse_query_events(str_event)  # TODO: add query events
                 events = self.parse_story_events(str_story)
+                pos = self.locate_event(qe2, events)
+                if not is_test:
+                    if 'before' in temp_rel:
+                        idx1, idx2 = pos, pos + 1
+                        events.insert(pos, qe1)
+                    else:
+                        idx1, idx2 = pos + 1, pos
+                        events.insert(pos + 1, qe1)
+                else:  # for test instances, do not insert the implicit event
+                    idx1, idx2 = -1, pos
+                instance = {
+                    'raw': line,
+                    'qe1': qe1,
+                    'qe2': qe2,
+                    'qe1_idx': idx1,
+                    'qe2_idx': idx2,
+                    'chain': events,
+                }
                 n_events += len(events)
-                fout.write(json.dumps(events) + '\n')
+                fout.write(json.dumps(instance) + '\n')
 
         print()
         print(f'avg_event_per_instance: {n_events / len(lines)}')
@@ -177,10 +233,12 @@ def main():
     try:
         for f in os.listdir(args.input_dir):
             if f.endswith('.txt'):
+                assert ('train' in f) ^ ('test' in f)
+                is_test = 'test' in f
                 input_path = os.path.join(args.input_dir, f)
                 output_path = os.path.join(args.output_dir, f.replace('.txt', '.jsonl'))
                 make_dir(output_path)
-                extractor.process_file(input_path, output_path)
+                extractor.process_file(input_path, output_path, is_test=is_test)
     finally:
         extractor.free()
 
